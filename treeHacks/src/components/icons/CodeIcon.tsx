372a5788a4f0ba999fa3776bdd3574c057643e7b
functionzz <164675620+functionzz@users.noreply.github.com>
2026-02-28 19:56:09 -0500

    add code block connections

---
+++ treeHacks/src/components/icons/CodeIcon.tsx
@@ -0,0 +1,13 @@
+export function CodeIcon() {
+	return (
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M5.5 4L2 8L5.5 12M10.5 4L14 8L10.5 12M9 2L7 14"
+				stroke="currentColor"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	)
+}
